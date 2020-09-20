eb5d34623fed245083e500b0abbe56adedb41683
Claudia Hauff <claudia.hauff@gmail.com>
2020-09-21 00:16:41 +0200

    Update app.js

--- node-sessions-ex/app.js
+++ node-sessions-ex/app.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var http = require("http");
+
+/* Cookie and session setup */
 var credentials = require("./credentials");
 var cookies = require("cookie-parser");
 var sessions = require("express-session");
+/* Setup complete */
 var app = express();
 
 app.use(cookies(credentials.cookieSecret));
@@ -16,13 +19,16 @@ app.use(sessions(sessionConfiguration));
 http.createServer(app).listen(3000);
 
 app.get("/countMe", function (req, res) {
+
+  /* session object available on req object only */
 	var session = req.session;
-	if (session.views) {
+
+	if (session.views) { /* the session exists! */
 		session.views++;
 		res.send("You have been here " + session.views + " times (last visit: " + session.lastVisit + ")");
 		session.lastVisit = new Date().toLocaleDateString();
 	}
-	else {
+	else { /* the session does not exist */
 		session.views = 1;
 		session.lastVisit = new Date().toLocaleDateString();
 		res.send("This is your first visit!");
