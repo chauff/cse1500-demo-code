21bac2ee1ab9053ce06bd166cd355e81a56a67dd
Claudia Hauff <claudia.hauff@gmail.com>
2020-09-20 11:01:11 +0200

    Update app.js

--- node-ejs-ex/app.js
+++ node-ejs-ex/app.js
@@ -11,14 +11,15 @@ http.createServer(app).listen(port, function () {
   console.log("Ready on port " + port);
 });
 
-var todos = [];
-todos.push({ message: 'Final exam', dueDate: 'January 2016'  });
-todos.push({ message: 'Prepare for assignment 6', dueDate: '05/01/2016' });
-todos.push({ message: 'Sign up for final exam', dueDate: '06/01/2016' });
+var wishlist = [];
+wishlist.push({ name: 'Wingspan', type: 'game night'  });
+wishlist.push({ name: 'Munchkin', type: 'party game' });
+wishlist.push({ name: 'Scrabble', type: 'game night' });
+wishlist.push({ name: 'Uno', type: 'party game' });
  
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get("/todos", function (req, res) {
-  res.render('todos', { title: 'My list of TODOs', todo_array: todos });
+app.get("/wishlist", function (req, res) {
+  res.render('wishlist', { title: 'Game wishlist', input: wishlist });
 });
