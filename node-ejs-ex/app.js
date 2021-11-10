18b948a1fa542a20ee2ca2bb11fe29af0a0df9cb
Claudia Hauff <claudia.hauff@gmail.com>
2021-11-10 11:24:25 +0100

    var to const

--- node-ejs-ex/app.js
+++ node-ejs-ex/app.js
@@ -1,22 +1,21 @@
 /* global __dirname */
 /* global process */
-var express = require("express");
-var url = require("url");
-var http = require("http");
-var app;
+const express = require("express");
+const url = require("url");
+const http = require("http");
 
-var port = process.argv[2];
-app = express();
+const port = process.argv[2];
+const app = express();
 http.createServer(app).listen(port, function () {
   console.log("Ready on port " + port);
 });
 
-var wishlist = [];
+const wishlist = [];
 wishlist.push({ name: 'Wingspan', type: 'game night'  });
 wishlist.push({ name: 'Munchkin', type: 'party game' });
 wishlist.push({ name: 'Scrabble', type: 'game night' });
 wishlist.push({ name: 'Uno', type: 'party game' });
- 
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
