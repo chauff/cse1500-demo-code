7e0201bdaf406f3ca5258a65bebe93ce824bd255
Claudia Hauff <claudia.hauff@gmail.com>
2021-12-26 17:08:13 +0100

    var to const

--- node-cookies-ex/app.js
+++ node-cookies-ex/app.js
@@ -1,17 +1,17 @@
-var express = require("express");
-var http = require("http");
-var credentials = require("./credentials");
-var cookies = require("cookie-parser");
+const express = require("express");
+const http = require("http");
+const credentials = require("./credentials");
+const cookies = require("cookie-parser");
 
 console.log("credentials: " + credentials.cookieSecret);
-var app = express();
+const app = express();
 app.use(cookies(credentials.cookieSecret));
 
 if (process.argv.length < 3) {
   console.log("Usage: node app.js <port>");
   process.exit(1);
 }
-var port = process.argv[2];
+const port = process.argv[2];
 http.createServer(app).listen(port, function () {
   console.log("Listening on port " + port);
 });
@@ -20,7 +20,7 @@ app.get("/sendMeCookies", function (req, res) {
   console.log("Handing out cookies");
   res.cookie("chocolate", "kruemel");
   res.cookie("signed_chocolate", "monster", { signed: true });
-  res.send();
+  res.send("Cookies received. Head to your web developer tools and take a look at the 'Storage' tab. Inside this tab, locate the 'Cookies' storage.");
 });
 
 app.get("/listAllCookies", function (req, res) {
